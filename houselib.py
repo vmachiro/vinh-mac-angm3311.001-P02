947c09fb2c3c8224521897ce2c7a6e05e1e94944
Vinh Mac <hhm220000@utdallas.edu>
2025-11-17 17:48:58 -0600

    WHOOPS WRONG FILE DO NOT OVERWRITE

--- houselib.py
+++ houselib.py
@@ -5,36 +5,60 @@ from PySide2.QtCore import Qt
 import maya.OpenMayaUI as omui
 from shiboken2 import wrapInstance
 
+
+import random
 import maya.cmds as cmds
-import houselib as hs
+
 
 def get_maya_main_win():
     main_win = omui.MQtUtil.mainWindow()
     return wrapInstance(int(main_win), QtWidgets.QWidget)
 
-class GridGenWin(QtWidgets.QDialog):
+class HouseGenWin(QtWidgets.QDialog):
+    """House Window Class"""
+
     def __init__(self):
+        # runs the init code of the parent QDialog class
         super().__init__(parent=get_maya_main_win())
-        self.gridGen = Grid()
-        self.setWindowTitle("Grid Generator")
+        self.houseGen = House()
+        self.setWindowTitle("House Generator")
         self.resize(500, 200)
         self._mk_main_layout()
         self._connect_signals()
 
     def _connect_signals(self):
+        self.number_of_floors_slider.valueChanged.connect(self._update_floors)
+        self.wall_height_slider.valueChanged.connect(self._update_walls)        
+        self.roof_height_slider.valueChanged.connect(self._update_roof)        
+        self.enable_grp_name_cb.stateChanged.connect(self.toggle_grpname)
+        self.enable_rand_cb.stateChanged.connect(self.toggle_random)
         self.clear_btn.clicked.connect(self.clear)
         self.cancel_btn.clicked.connect(self.cancel)
         self.build_btn.clicked.connect(self.build)
-        self.enable_grp_name_cb.stateChanged.connect(self.toggle_grpname)
+
+    def _update_floors(self, value):
+        self.floor_result_lbl.setText(f'Current Value: {value}')
+    
+    def _update_walls(self, value):
+        self.wall_result_lbl.setText(f'Current Value: {value}')
+
+    def _update_roof(self, value):
+        self.roof_result_lbl.setText(f'Current Value: {value}')
 
     @QtCore.Slot()
     def toggle_grpname(self):
         is_custom_grpname_enabled = self.enable_grp_name_cb.isChecked()
         self.grp_name_ledit.setDisabled(not is_custom_grpname_enabled)
+    
+    @QtCore.Slot()
+    def toggle_random(self):
+        is_random_houses_enabled = self.enable_rand_cb.isChecked()
+        self.number_of_floors_slider.setDisabled(is_random_houses_enabled)
+        return is_random_houses_enabled
 
     @QtCore.Slot()
     def clear(self):
-        cmds.select(cmds.ls(self.gridGen.grpname+"*"))        
+        cmds.select(cmds.ls(self.houseGen.housename+"*"))        
         cmds.delete()
 
     @QtCore.Slot()
@@ -43,12 +67,22 @@ class GridGenWin(QtWidgets.QDialog):
 
     @QtCore.Slot()
     def build(self):
-        self._update_grid_properties()
-        self.gridGen.build()
-    
-    def _update_grid_properties(self):
-        self.gridGen.__init__() # reset properties to default
-        self.gridGen.grpname = self.grp_name_ledit.text()
+        self._update_housegen_properties()
+        self.houseGen.build()
+
+    def _update_housegen_properties(self):
+        self.houseGen.__init__() # reset properties to default
+        self.houseGen.number_of_houses = self.number_of_houses_spnbox.value()
+        self.houseGen.roof_height = self.roof_height_slider.value()        
+        self.houseGen.wall_height = self.wall_height_slider.value()
+        self.houseGen.number_of_floors = self.number_of_floors_slider.value()
+        self.houseGen.number_of_windows = self.number_of_windows_spnbox.value()
+        self.houseGen.number_of_doors = self.door_spnbox.value()
+        self.houseGen.housename = self.grp_name_ledit.text()
+
+        if self.toggle_random() == True:
+            random_floor_num = random.randint(1,5)
+            self.houseGen.number_of_floors = random_floor_num
 
     def _mk_main_layout(self):
         self.main_layout = QtWidgets.QVBoxLayout()
@@ -59,24 +93,82 @@ class GridGenWin(QtWidgets.QDialog):
 
     def _add_form_layout(self):
         self.form_layout = QtWidgets.QFormLayout()
+        self._add_houses()
+        self._add_roof_height()
+        self._add_wall_height()
+        self._add_floors()     
+        self._add_randomization()        
+        self._add_windows()
+        self._add_doors()
         self._add_custom_grpname()
-        self.main_layout.addLayout(self.form_layout)    
+        self.main_layout.addLayout(self.form_layout)
+
+    def _add_randomization(self):
+        self.enable_rand_cb = QtWidgets.QCheckBox("Choose floors for me!")
+        self.form_layout.addRow(self.enable_rand_cb)
+
+    def _add_houses(self):
+        self.number_of_houses_spnbox = QtWidgets.QSpinBox()
+        self.number_of_houses_spnbox.setValue(1)
+        self.form_layout.addRow("Number of Houses", self.number_of_houses_spnbox)
 
     def _add_custom_grpname(self):
-        self.enable_grp_name_cb = QtWidgets.QCheckBox("Enable Custom Grid Name")
-        self.grp_name_ledit = QtWidgets.QLineEdit("Grid")
+        self.enable_grp_name_cb = QtWidgets.QCheckBox("Enable Custom House Name")
+        self.grp_name_ledit = QtWidgets.QLineEdit("House")
         self.grp_name_ledit.setDisabled(True)
         self.form_layout.addRow(self.enable_grp_name_cb)
         self.form_layout.addRow("Group", self.grp_name_ledit)
 
+    def _add_windows(self):
+        self.number_of_windows_spnbox = QtWidgets.QSpinBox()
+        self.number_of_windows_spnbox.setValue(2)
+        self.number_of_windows_spnbox.setMaximum(4)
+        self.form_layout.addRow("Windows per floor", self.number_of_windows_spnbox)
+
+    def _add_floors(self):
+        self.number_of_floors_slider = QtWidgets.QSlider(Qt.Orientation.Horizontal, self)
+        self.number_of_floors_slider.setRange(1,10)
+        self.number_of_floors_slider.setValue(1)    
+        self.form_layout.addRow("Number of Floors", self.number_of_floors_slider)
+
+        self.floor_result_lbl = QtWidgets.QLabel('', self)
+        self.floor_result_lbl.setAlignment(Qt.AlignCenter)
+        self.form_layout.addRow(self.floor_result_lbl)
+
+    def _add_roof_height(self):
+        self.roof_height_slider = QtWidgets.QSlider(Qt.Orientation.Horizontal, self)
+        self.roof_height_slider.setRange(0,5)
+        self.roof_height_slider.setValue(1)          
+        self.form_layout.addRow("Roof Height", self.roof_height_slider)
+
+        self.roof_result_lbl = QtWidgets.QLabel('', self)
+        self.roof_result_lbl.setAlignment(Qt.AlignCenter)
+        self.form_layout.addRow(self.roof_result_lbl)
+
+    def _add_doors(self):
+        self.door_spnbox = QtWidgets.QSpinBox()
+        self.door_spnbox.setValue(1)
+        self.door_spnbox.setRange(0,2)
+        self.form_layout.addRow("Door Number", self.door_spnbox)
+
+    def _add_wall_height(self):
+        self.wall_height_slider = QtWidgets.QSlider(Qt.Orientation.Horizontal, self)
+        self.wall_height_slider.setValue(5)
+        self.wall_height_slider.setRange(5,20)
+        self.form_layout.addRow("Wall Height", self.wall_height_slider)
+
+        self.wall_result_lbl = QtWidgets.QLabel('', self)        
+        self.wall_result_lbl.setAlignment(Qt.AlignCenter)
+        self.form_layout.addRow(self.wall_result_lbl)
+
     def _add_name_label(self):
-        self.name_lbl = QtWidgets.QLabel("Grid Generator")
-        self.name_lbl.setStyleSheet("background-color: blue;"
+        self.name_lbl = QtWidgets.QLabel("House Generator")
+        self.name_lbl.setStyleSheet("background-color: purple;"
                                     "color: white;"
                                     "font: bold 24px;")
         self.name_lbl.setAlignment(Qt.AlignCenter)
         self.main_layout.addWidget(self.name_lbl)
- 
+
     def _mk_btn_layout(self):
         self.btn_layout = QtWidgets.QHBoxLayout()
         self.build_btn = QtWidgets.QPushButton("Build")
@@ -87,64 +179,178 @@ class GridGenWin(QtWidgets.QDialog):
         self.btn_layout.addWidget(self.cancel_btn)
         self.main_layout.addLayout(self.btn_layout)
 
+class House():
 
-class Grid():
     def __init__(self):
+        self.number_of_houses = 1
+        self.number_of_floors = 2        
+        self.wall_height = 5 
+        self.house_width = 5
+        self.roof_height = 1 
+        self.number_of_windows = 4
+        self.number_of_doors = 1
+        self.housename = "House"
+    
+    def get_height_of_house(self):
+        return self.wall_height * self.number_of_floors
+
+    def get_curent_floor(self, floor_num):
+        return self.wall_height*(floor_num)
+
+    def get_base_of_house(self):
+        base_height = self.wall_height/self.get_height_of_house() * self.number_of_floors
+        return base_height
+
+    def get_window_height_from_base(self):
+        window_placement = self.wall_height/self.get_height_of_house() + (self.wall_height/2)
+        return window_placement
+
+    def get_center_of_wall(self):
+        return self.house_width/2
+    
+    def mkhousebody(self):
+        xform, shape = cmds.polyCube(height= self.get_height_of_house(),
+                                    width = self.house_width,
+                                    depth = self.house_width,
+                                    name = "housebody")
         
-        self.grpname = "Grid"
-        self.population_scale = 3 # this determines how many houses are on each square/grid
-        self.roads = 0 # for now, this just determines whether or not there are roads
+        cmds.xform(xform, translation = [0,self.get_height_of_house()/2,0])          
+        
+        return xform
 
-    def create_plane(self, num_of_houses, house_width):
-       
-        scale = house_width*num_of_houses
-        cmds.polyPlane(n='plane', sx=4, sy=4, w=(scale)*3, h=(scale)*2)
+    def mkwindows(self):
+        window_GRP = []
 
-    #   for every population_scale, add another subdivision/scale the plane larger? 
-    #   OR repeat the loop of creating and populating the plane with houses and shift the duplicate to the side
+        for floor_num in range(self.number_of_floors):
+            for windows_num in range(self.number_of_windows):
+                xform, shape = cmds.polyCube(height= 2,
+                                            width = 1,
+                                            depth = 0.5,
+                                            name = "window"+str(windows_num))
+                
+                self.transform_window(xform)
+                world_pos = cmds.xform(xform, query=True, worldSpace=True, translation=True)
 
-    def place_house(self):
-        house1 = hs.House()
-        house1.number_of_houses = 3
-        house1.build()
+                if self.number_of_windows > 2:
+                    if windows_num > 1:
+                        cmds.xform( r=True, ro=(0,90,0) )
+                        x_pos = world_pos[2] 
+                        cmds.move( x_pos, x=True )
 
-        # this is the only method that currently uses the house instance, so we should use it to send info to the scale method
-        self.create_plane(house1.number_of_houses, house1.house_width)
+                        z_pos = world_pos[0]
+                        cmds.move( z_pos, z=True )
 
-        return house1
+                        if windows_num%2 == 1:
+                            cmds.move( x_pos*-1, x=True )
+                            cmds.move( z_pos*-1, z=True )
+                    
+                    world_pos = cmds.xform(xform, query=True, worldSpace=True, translation=True)
 
-    def rotate_house(self):
-        # TODO:
-        # define a method that flips the groups of houses to face different directions
-        #   can probably reuse the window rotation code
-        pass
+                if windows_num%2 == 1:
+                    self.transform_window_to_back(world_pos[2])        
+                    world_pos = cmds.xform(xform, query=True, worldSpace=True, translation=True)
 
-    def make_road(self):
-        # TODO:
-        # (LAST) define a method that puts a road on the map around the houses. 
-        #   probably just a straight path for minimalization
-        pass
+                if floor_num > 0:
+                    self.transform_window_up(floor_num, xform, world_pos) 
+                
+                window_GRP.append(xform)  
 
-    def transform_row(self):
-        cmds.move( 8, z=True )
+        return window_GRP
+
+    def transform_window_up(self, floor_num, xform, world_pos):
+        pos = [world_pos[0], (world_pos[1]+(self.get_curent_floor(floor_num))), world_pos[2]]
+        cmds.xform( xform, translation=pos )
+
+    def transform_window(self, window):
+        z_pos = self.get_center_of_wall()
+        y_pos = self.get_window_height_from_base()
+        pos = [0, y_pos, z_pos]
+
+        cmds.xform(window, translation=pos)
+
+    def transform_window_to_back(self, window_z_pos):
+        z_pos = window_z_pos*-1
+
+        cmds.move( z_pos, z=True )
+
+    def mkdoors(self):
+        door_GRP = []        
+        
+        for door_num in range(self.number_of_doors):
+            xform, shape = cmds.polyCube(height= self.wall_height/4,
+                                        width = self.house_width/10,
+                                        depth = .5,
+                                        name = "door"+str(door_num))
+            
+            self.transform_door(xform)
+            
+            if door_num > 0:
+                self.transform_door_to_back(xform)
+            
+            door_GRP.append(xform)
+        
+        return door_GRP
+
+    def mkhouseflatroof(self):
+        xform, shape = cmds.polyCube(height= self.roof_height,
+                                    width = self.house_width*1.25,
+                                    depth = self.house_width*1.25,
+                                    name = "roof")
+
+        cmds.xform(xform, translation = [0,self.get_height_of_house(),0])
+
+        return xform
+    
+    def transform_door(self, door):
+        z_pos = self.get_center_of_wall()
+        y_pos = self.get_base_of_house()*.7
+        pos = [0, y_pos, z_pos]
+
+        cmds.xform(door, translation=pos)
+
+    def transform_door_to_back(self, door):
+        z_pos = self.get_center_of_wall()
+        y_pos = self.get_base_of_house()
+        pos = [0, y_pos, z_pos*-1]
+
+        cmds.xform(door, translation=pos)
+
+    def transform_house(self, house_x_pos, house_num, housename):
+        x_pos = house_x_pos + self.house_width*house_num*1.5
+
+        cmds.move( x_pos, housename, x=True )
 
     def build(self):
 
-        grid_list= []
+        house_things = []
 
-        for scale_num in range(self.population_scale):
+        for house_num in range(self.number_of_houses):
+            house_name = self.housename+str(house_num) 
+
+            housebody = self.mkhousebody()
+            house_things.append(housebody)
+
+            if self.roof_height != 0:
+                houseroof = self.mkhouseflatroof()
+                house_things.append(houseroof)
             
-            self.place_house()
-            cmds.select( all=True )
-            self.transform_row()
-            # rotate if necessary
-            # (polish) add roads
+            cmds.group(house_things, name=house_name)
+            house_things.clear()
 
-
-# POLISH:
-# figure out house randomization
-# user customizability of the houses comes after roads 
-# GUI should call the house generator at some point with a button, so that the user can fine tune how the houses look
+            doors_grp = self.mkdoors()
+            cmds.group(doors_grp, name="doors_GRP", parent=house_name)
+            
+            windows_grp = self.mkwindows()
+            cmds.group(windows_grp, name="windows_GRP", parent=house_name)
+            
+            world_pos = cmds.xform(house_name, query=True, worldSpace=True, translation=True)
+      
+            self.transform_house(world_pos[0],house_num,house_name)
+            
+            cmds.makeIdentity(house_name, apply=True, translate=True, rotate=True, 
+                            scale=True, normal=False, preserveNormals=True)
+            
 
 if __name__ == "__main__":
     pass
+     
