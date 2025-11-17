41e7de136e14d3289f148ce11243f0720ba09dc4
Vinh Mac <hhm220000@utdallas.edu>
2025-11-17 15:08:01 -0600

    forgot to update last, here's the same changes as before but i saved it correctly from the local file

--- maplib.py
+++ maplib.py
@@ -11,23 +11,15 @@ class Grid():
         self.population_scale = 1 # this determines how many houses are on each square/grid
         self.roads = 0 # for now, this just determines whether or not there are roads
 
-    # TODO:
-    # (FIRST) define a method that makes the flat plane that the houses sit on
     def create_plane(self):
         cmds.plane( p=(1, 1, 1), s=10 )
     #   transform the plane to be on level with the houses. or vice versa
     #   for every population_scale, add another subdivision/scale the plane larger? 
     #   OR repeat the loop of creating and populating the plane with houses and shift the duplicate to the side
 
-    # TODO:
-    # define a method that populates the houses onto the map using House()
     def place_house(self):
-        #   instance a House() object
         house1 = hs.House()
-        # set the variables for default testing
-        #   make. the houses based on set default parameters within the class
         house1.number_of_houses = 3
-        #   build the houses
         house1.build()
 
     def rotate_house(self):
@@ -43,20 +35,22 @@ class Grid():
         pass
 
     def build(self):
-        # make the plane
         self.create_plane()
-        # make a list to later use to group the houses with the grid
+
         grid_list= []
-        # loop through the population scale in order to make tons of houses using House()
+
         for scale_num in range(self.population_scale):
-            # make houses
             self.place_house()
             # transform if needed
-            # append house thing to list
+
             grid_list.append()
             # rotate if necessary
             # (polish) add roads
-        # freeze transforms 
+
+        cmds.group(grid_list, name=Grid)
+
+        cmds.makeIdentity(grid_list, apply=True, translate=True, rotate=True, 
+                            scale=True, normal=False, preserveNormals=True)
 
 
 # POLISH:
